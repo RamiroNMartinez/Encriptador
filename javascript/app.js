36088a9566e8edf89583045fc4290b074eb93473
Ramiro Martínez <ramiro.nicolas.martinez14@gmail.com>
2024-08-16 16:08:02 -0300

    Agrego funcionalidad

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+
+function Encriptar(){
+
+    const palabraEntrada = document.getElementById('Entrada');
+    console.log(palabraEntrada);
+}
+
+function Desencriptar(){
+
+    const palabraEntrada = document.getElementsById('Entrada');
+    console.log(palabraEntrada);
+}
